2a5fa35f846d783ce84e43eaa8772c4f32496cf5
liliang2655 <925918588@qq.com>
2024-09-13 16:49:41 +0800

    ll

---
+++ LLEN/Enhancement/test_from_dataset.py
@@ -0,0 +1,302 @@
+# 一个用于图像增强的Python脚本，它使用混合的CNN和Transformer模型（例如基于basicsr库）来处理图像数据，并计算图像质量指标，如PSNR和SSIM。这段代码具体涵盖了参数解析、模型配置和图像处理的多个步骤。
+from ast import arg
+import numpy as np
+import os
+import argparse
+from tqdm import tqdm
+import cv2
+
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import utils
+
+from natsort import natsorted
+from glob import glob
+from skimage import img_as_ubyte
+from pdb import set_trace as stx
+from skimage import metrics
+
+from basicsr.models import create_model
+from basicsr.utils.options import dict2str, parse
+
+def self_ensemble(x, model):
+    def forward_transformed(x, hflip, vflip, rotate, model):
+        if hflip:
+            x = torch.flip(x, (-2,))
+        if vflip:
+            x = torch.flip(x, (-1,))
+        if rotate:
+            x = torch.rot90(x, dims=(-2, -1))
+        x = model(x)
+        if rotate:
+            x = torch.rot90(x, dims=(-2, -1), k=3)
+        if vflip:
+            x = torch.flip(x, (-1,))
+        if hflip:
+            x = torch.flip(x, (-2,))
+        return x
+    t = []
+    for hflip in [False, True]:
+        for vflip in [False, True]:
+            for rot in [False, True]:
+                t.append(forward_transformed(x, hflip, vflip, rot, model))
+    t = torch.stack(t)
+    return torch.mean(t, dim=0)
+
+# 参数解析：使用argparse库来解析命令行输入的参数，这些参数控制模型的加载、数据来源、结果保存的位置等。
+parser = argparse.ArgumentParser(description='Image Enhancement using LLEN')
+
+# 目录存放待增强的图片
+parser.add_argument('--input_dir', default='./Enhancement/Datasets', type=str, help='Directory of validation images')
+# 模型输出结果保存的目录
+parser.add_argument('--result_dir', default='./results/', type=str, help='Directory for results')
+parser.add_argument('--output_dir', default='', type=str, help='Directory for output')
+parser.add_argument('--opt', type=str, default='Options/SDSD_indoor.yml', help='Path to option YAML file.')
+# 更多参数定义，包括模型权重路径、配置文件路径、测试数据集等。
+# 预训练模型权重文件
+parser.add_argument('--weights', default='pretrained_weights/SDSD_indoor.pth', type=str, help='Path to weights')
+# 测试所用的数据集名称
+parser.add_argument('--dataset', default='SDSD_indoor', type=str, help='Test Dataset') 
+parser.add_argument('--gpus', type=str, default="0", help='GPU devices.')
+parser.add_argument('--GT_mean', action='store_true', help='Use the mean of GT to rectify the output of the model')
+parser.add_argument('--self_ensemble', action='store_true', help='Use self-ensemble to obtain better results')
+
+# 解析这些参数。
+args = parser.parse_args()
+
+# 设置使用哪些GPU，例如"0,1"表示同时使用第0和第1号GPU。
+gpu_list = ','.join(str(x) for x in args.gpus)
+os.environ['CUDA_VISIBLE_DEVICES'] = gpu_list
+# 指定使用的GPU
+print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
+
+####### Load yaml #######
+# 从指定的yaml文件加载模型配置。
+yaml_file = args.opt
+weights = args.weights
+print(f"dataset {args.dataset}")
+
+import yaml
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+# 不是训练模式，是测试或推理模式。
+opt = parse(args.opt, is_train=False)
+opt['dist'] = False
+
+x = yaml.load(open(args.opt, mode='r'), Loader=Loader)
+s = x['network_g'].pop('type')
+##########################
+
+# 根据配置创建模型实例。
+model_restoration = create_model(opt).net_g
+
+# 加载预训练权重到模型。
+checkpoint = torch.load(weights)
+
+try:
+    model_restoration.load_state_dict(checkpoint['params'])
+except:
+    new_checkpoint = {}
+    for k in checkpoint['params']:
+        new_checkpoint['module.' + k] = checkpoint['params'][k]
+    model_restoration.load_state_dict(new_checkpoint)
+
+print("===>Testing using weights: ", weights)
+# 将模型移动到GPU上。
+model_restoration.cuda()
+# 如果有多GPU，使用数据并行。
+model_restoration = nn.DataParallel(model_restoration)
+# 设定模型为评估模式，不启用dropout等训练时才有的特性。
+model_restoration.eval()
+
+# 生成输出结果的文件
+# 准备数据集路径和输出结果的目录。
+# 用于图像尺寸对齐的因子。
+factor = 4
+dataset = args.dataset
+config = os.path.basename(args.opt).split('.')[0]
+checkpoint_name = os.path.basename(args.weights).split('.')[0]
+result_dir = os.path.join(args.result_dir, dataset, config, checkpoint_name)
+# 原始输入图像保存目录。
+result_dir_input = os.path.join(args.result_dir, dataset, 'input')
+# 真实图像（ground truth）保存目录。
+result_dir_gt = os.path.join(args.result_dir, dataset, 'gt')
+output_dir = args.output_dir
+# stx()
+os.makedirs(result_dir, exist_ok=True)
+if args.output_dir != '':
+    os.makedirs(output_dir, exist_ok=True)
+
+# 开始测试循环。
+psnr = []
+ssim = []
+if dataset in ['SID', 'SMID', 'SDSD_indoor', 'SDSD_outdoor']:
+    # 创建必要的目录。
+    os.makedirs(result_dir_input, exist_ok=True)
+    os.makedirs(result_dir_gt, exist_ok=True)
+    if dataset == 'SID':
+        from basicsr.data.SID_image_dataset import Dataset_SIDImage as Dataset
+    elif dataset == 'SMID':
+        from basicsr.data.SMID_image_dataset import Dataset_SMIDImage as Dataset
+    else:
+        from basicsr.data.SDSD_image_dataset import Dataset_SDSDImage as Dataset
+    opt = opt['datasets']['val']
+    opt['phase'] = 'test'
+    if opt.get('scale') is None:
+        opt['scale'] = 1
+    if '~' in opt['dataroot_gt']:
+        opt['dataroot_gt'] = os.path.expanduser('~') + opt['dataroot_gt'][1:]
+    if '~' in opt['dataroot_lq']:
+        opt['dataroot_lq'] = os.path.expanduser('~') + opt['dataroot_lq'][1:]
+    # 创建数据加载器，从数据集中获取数据。
+    # 实例化数据集类。
+    dataset = Dataset(opt)
+    print(f'test dataset length: {len(dataset)}')
+    dataloader = DataLoader(dataset=dataset, batch_size=1, shuffle=False)
+    with torch.inference_mode():
+        # 遍历数据加载器中的每个批次。
+        for data_batch in tqdm(dataloader):
+            torch.cuda.ipc_collect()
+            torch.cuda.empty_cache()
+
+            # 对图像进行预处理，确保尺寸是对齐的。
+            # 低质量图像。
+            input_ = data_batch['lq']
+            input_save = data_batch['lq'].cpu().permute(
+                0, 2, 3, 1).squeeze(0).numpy()
+            # 目标高质量图像。
+            target = data_batch['gt'].cpu().permute(
+                0, 2, 3, 1).squeeze(0).numpy()
+            inp_path = data_batch['lq_path'][0]
+
+            # Padding in case images are not multiples of 4
+            # 获取图像高度和宽度。
+            h, w = input_.shape[2], input_.shape[3]
+            # 调整尺寸。
+            H, W = ((h + factor) // factor) * factor, ((w + factor) // factor) * factor
+            # 计算需要填充的大小。
+            padh = H - h if h % factor != 0 else 0
+            padw = W - w if w % factor != 0 else 0
+            # 填充图像边缘。
+            input_ = F.pad(input_, (0, padw, 0, padh), 'reflect')
+
+            if args.self_ensemble:
+                restored = self_ensemble(input_, model_restoration)
+            else:
+                # 应用模型进行图像增强。
+                restored = model_restoration(input_)
+
+            # Unpad images to original dimensions
+            # 删除填充，恢复原始尺寸。
+            restored = restored[:, :, :h, :w]
+
+            # 将结果限制在0-1之间，并转为numpy数组。
+            restored = torch.clamp(restored, 0, 1).cpu().detach().permute(0, 2, 3, 1).squeeze(0).numpy()
+
+            # 计算并记录性能指标。
+            # 如果设置了基于平均灰度值的调整。
+            if args.GT_mean:
+                # This test setting is the same as KinD, LLFlow, and recent diffusion models
+                # Please refer to Line 73 (https://github.com/zhangyhuaee/KinD/blob/master/evaluate_LOLdataset.py)
+                mean_restored = cv2.cvtColor(restored.astype(np.float32), cv2.COLOR_BGR2GRAY).mean()
+                mean_target = cv2.cvtColor(target.astype(np.float32), cv2.COLOR_BGR2GRAY).mean()
+                restored = np.clip(restored * (mean_target / mean_restored), 0, 1)
+
+            psnr.append(utils.PSNR(target, restored))
+            ssim.append(utils.calculate_ssim(
+                img_as_ubyte(target), img_as_ubyte(restored)))
+            type_id = os.path.dirname(inp_path).split('/')[-1]
+            os.makedirs(os.path.join(result_dir, type_id), exist_ok=True)
+            os.makedirs(os.path.join(result_dir_input, type_id), exist_ok=True)
+            os.makedirs(os.path.join(result_dir_gt, type_id), exist_ok=True)
+            utils.save_img((os.path.join(result_dir, type_id, os.path.splitext(
+                os.path.split(inp_path)[-1])[0] + '.png')), img_as_ubyte(restored))
+            utils.save_img((os.path.join(result_dir_input, type_id, os.path.splitext(
+                os.path.split(inp_path)[-1])[0] + '.png')), img_as_ubyte(input_save))
+            utils.save_img((os.path.join(result_dir_gt, type_id, os.path.splitext(
+                os.path.split(inp_path)[-1])[0] + '.png')), img_as_ubyte(target))
+else:
+
+    input_dir = opt['datasets']['val']['dataroot_lq']
+    target_dir = opt['datasets']['val']['dataroot_gt']
+    print(input_dir)
+    print(target_dir)
+
+    input_paths = natsorted(
+        glob(os.path.join(input_dir, '*.png')) + glob(os.path.join(input_dir, '*.jpg')))
+
+    target_paths = natsorted(glob(os.path.join(
+        target_dir, '*.png')) + glob(os.path.join(target_dir, '*.jpg')))
+
+    with torch.inference_mode():
+        for inp_path, tar_path in tqdm(zip(input_paths, target_paths), total=len(target_paths)):
+
+            torch.cuda.ipc_collect()
+            torch.cuda.empty_cache()
+
+            img = np.float32(utils.load_img(inp_path)) / 255.
+            target = np.float32(utils.load_img(tar_path)) / 255.
+
+            img = torch.from_numpy(img).permute(2, 0, 1)
+            input_ = img.unsqueeze(0).cuda()
+
+            # Padding in case images are not multiples of 4
+            b, c, h, w = input_.shape
+            H, W = ((h + factor) // factor) * \
+                factor, ((w + factor) // factor) * factor
+            padh = H - h if h % factor != 0 else 0
+            padw = W - w if w % factor != 0 else 0
+            input_ = F.pad(input_, (0, padw, 0, padh), 'reflect')
+
+            if h < 3000 and w < 3000:
+                if args.self_ensemble:
+                    restored = self_ensemble(input_, model_restoration)
+                else:
+                    restored = model_restoration(input_)
+            else:
+                # split and test
+                input_1 = input_[:, :, :, 1::2]
+                input_2 = input_[:, :, :, 0::2]
+                if args.self_ensemble:
+                    restored_1 = self_ensemble(input_1, model_restoration)
+                    restored_2 = self_ensemble(input_2, model_restoration)
+                else:
+                    restored_1 = model_restoration(input_1, model_restoration)
+                    restored_2 = model_restoration(input_2, model_restoration)
+                restored = torch.zeros_like(input_)
+                restored[:, :, :, 1::2] = restored_1
+                restored[:, :, :, 0::2] = restored_2
+
+            # Unpad images to original dimensions
+            restored = restored[:, :, :h, :w]
+
+            restored = torch.clamp(restored, 0, 1).cpu(
+            ).detach().permute(0, 2, 3, 1).squeeze(0).numpy()
+
+            if args.GT_mean:
+                # This test setting is the same as KinD, LLFlow, and recent diffusion models
+                # Please refer to Line 73 (https://github.com/zhangyhuaee/KinD/blob/master/evaluate_LOLdataset.py)
+                mean_restored = cv2.cvtColor(restored.astype(np.float32), cv2.COLOR_BGR2GRAY).mean()
+                mean_target = cv2.cvtColor(target.astype(np.float32), cv2.COLOR_BGR2GRAY).mean()
+                restored = np.clip(restored * (mean_target / mean_restored), 0, 1)
+
+            psnr.append(utils.PSNR(target, restored))
+            ssim.append(utils.calculate_ssim(
+                img_as_ubyte(target), img_as_ubyte(restored)))
+            if output_dir != '':
+                utils.save_img((os.path.join(output_dir, os.path.splitext(
+                    os.path.split(inp_path)[-1])[0] + '.png')), img_as_ubyte(restored))
+            else:
+                utils.save_img((os.path.join(result_dir, os.path.splitext(
+                    os.path.split(inp_path)[-1])[0] + '.png')), img_as_ubyte(restored))
+
+psnr = np.mean(np.array(psnr))
+ssim = np.mean(np.array(ssim))
+print("PSNR: %f " % (psnr))
+print("SSIM: %f " % (ssim))
---
+++ LLEN/train_multigpu.sh
@@ -0,0 +1,10 @@
+
+config=$1
+gpu_ids=$2
+master_port=${3:-4321} # Please use different master_port for different training processes.
+
+gpu_count=$(echo $gpu_ids | tr -cd ',' | wc -c)
+gpu_count=$((gpu_count + 1))
+
+# pytorch1.x
+CUDA_VISIBLE_DEVICES=$gpu_ids python -m torch.distributed.launch --nproc_per_node=$gpu_count --master_port=$master_port basicsr/train.py --opt $config --launcher pytorch
